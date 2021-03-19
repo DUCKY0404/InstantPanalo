c5415156ebe0f43a98c6daabdfd2830f5edb2d69
Nash Ivan L Senobin <79844536+Nashensmirtz@users.noreply.github.com>
2021-03-19 19:38:20 +0800

    Nash added comment

--- src/Games/JoseRizal/pages/HomeScreen.js
+++ src/Games/JoseRizal/pages/HomeScreen.js
@@ -38,7 +38,7 @@ class DSHome extends Component {
         source={require('./images/bplain.jpg')}>
         {/* quitbutton ni rizal */}
 
-        {/* sample comment */}
+        {/* sample ni nash */}
         <TouchableOpacity
           onPress={() => navigate('HomeScreen')}
           style={styles.button_back}>
